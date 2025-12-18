8dbe67750c6d231d191ecc5a7416f34d8a5688ed
ALing-LingXi <2841572483@qq.com>
2025-12-18 21:42:37 +0800

    test1/test.js

--- test1/test.js
+++ test1/test.js
@@ -3,6 +3,7 @@ const app = express()
 app.get("/server",(req,res)=>{
   //设置响应头
   res.setHeader("Access-Control-Allow-Origin","*")
+  console.log("query:", req.query)
   res.send("Hello AJAX")
 })
 app.listen(3000,()=>{

05b845bfa089fe4a6024c846927c053d1acb6e26
ALing-LingXi <2841572483@qq.com>
2025-12-19 21:00:14 +0800

    test1/test2.js

--- test1/test2.js
+++ test1/test2.js
@@ -1,9 +1,23 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.post("/server",(req,res)=>{
   res.setHeader("Access-Control-Allow-Origin","*")
   const data={name:"xixihaha",age:"19"}
   res.send(JSON.stringify(data))
+})
+app.get("/delay",(req,res)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  setTimeout(()=>{
+    res.send("我去你🐎的课后延时服务");
+  },1000)
+})
+app.get("/server",(req,res)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  setTimeout(()=>{
+    res.send("我去你🐎的课后延时服务");
+  },1000)
 })
 app.listen(4000,()=>{
   console.log("启动成功");
